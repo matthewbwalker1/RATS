5ec7dd723281446b2eb7a55d2840471172117e79
Gavin Maxwell <gavin.maxwell13@gmail.com>
2026-03-17 22:05:42 -0600

    Resolution mapping, confirmed reports, and rates of reporting (all reports and confirmed reports)

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -8,8 +8,39 @@ library(readxl)
 report_data <- vroom("311_rodent_calls.csv")
 district_data <- read_xlsx("nyc_council_context.xlsx")
 
+# Resolution Description Mapping ----------------------------------------
 
-# Merging Data Sets -----------------------------------------------------
+resolution_descriptions <- c(
+  "The Department of Health and Mental Hygiene found violations on the property you reported. Follow-up inspections will be scheduled.",
+  "The Department of Health and Mental Hygiene tried to inspect the property you reported but could not access the site. If you have information that will help the inspector access the site, please go online to nyc.gov/311 or call 311 and refile your complaint with contact information.",
+  "This service request was closed because the Department of Health and Mental Hygiene received an earlier complaint about the same location.  You can find inspection results for this address by going to the online Rat Portal at www.nyc.gov/rats.",
+  "The Department of Health and Mental Hygiene inspected the property you reported and did not find any violations at the time of the inspection. The property passed inspection.",
+  "An inspection of the property was conducted and it passed with minor violations found.",
+  NA,
+  "An inspection of this property was conducted and violations were identified. The owner is correcting the condition. If the condition persists, please submit another complaint to 311.",
+  "The Department of Health and Mental Hygiene could not locate the address provided. Please go online to nyc.gov/311 or call 311 and file another request and provide the complete street address of the builiding. If your complaint was about a vacant lot, abandoned building ,street, or in a sewer, provide the nearest street address or intersection.",
+  "The Department of Health and Mental Hygiene closed this complaint and referred it to another agency because it was not within its jurisdiction.",
+  "The Department of Health and Mental Hygiene (DOHMH) actioned and closed your service request. No further updates will be available.",
+  "An investigation was performed on this property and the property owner was provided with information and guidance on eliminating rodents or rodent conditions on their property. If the condition persists, please submit another complaint to 311.",
+  "The Department of Health and Mental Hygiene could not conduct an inspection at the property you reported because dangerous conditions at that site make it unsafe for inspectors to attempt access."
+)
+
+resolution_statuses <- c(
+  "Confirmed", "No determination/administrative", "No determination/administrative", 
+  "Not Confirmed", "Not Confirmed", "No determination/administrative", 
+  "Confirmed", "No determination/administrative", "No determination/administrative",
+  "No determination/administrative", "Confirmed", "Not Confirmed"
+)
+
+res_map <- data.frame(resolution_description = resolution_descriptions,
+                      resolution_status = resolution_statuses)
+
+report_data <- report_data %>% 
+  left_join(res_map, 
+            join_by("Resolution Description" == "resolution_description"))
+
+
+# Merging Data Sets, counting reports by type, computing reporting rates--
 rat_data <- report_data %>%
   mutate(`Council District` = as.numeric(`Council District`)) %>%
   left_join(district_data,
@@ -18,10 +49,13 @@ rat_data <- report_data %>%
 council_data <- report_data %>%
   mutate(`Council District` = as.numeric(`Council District`)) %>%
   summarize(reports = n(),
+            confirmed_reports = sum(resolution_status == "Confirmed"),
             .by = `Council District`) %>%
   inner_join(district_data,
-            join_by("Council District" == "council_district_id")) %>%
-  select(-council_district_name)
+             join_by("Council District" == "council_district_id")) %>%
+  select(-council_district_name) %>%
+  mutate(rate_all = (reports/population) * 1000,
+         rate_confirmed = (confirmed_reports/population) * 1000)
 
 
 # Writing Data ----------------------------------------------------------
